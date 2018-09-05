a9c56dc85a70c1e45a080bc3589afef8c1b4ee18
AntonEmery <antonemery@gmail.com>
2018-09-04 17:17:03 -0700

    custom number working, added css

--- controllers/countController.js
+++ controllers/countController.js
@@ -2,14 +2,21 @@ const fs = require('fs');
 
 exports.increment = (req, res) => {
   var io = req.app.get('socketio');
-  let data = fs.readFileSync('./data.txt', 'utf8');
-  let number = parseInt(data);
-  number++;
-  fs.writeFile('./data.txt', number, function() {
-    io.emit('updateCounter', { data: number });
-    res.redirect('/');
-  });
-
+  if(req.body.custom_number) {
+    fs.writeFile('./data.txt', req.body.custom_number, function() {
+      io.emit('updateCounter', { data: req.body.custom_number });
+      res.redirect('/');
+      return;
+    });
+  } else {
+    let data = fs.readFileSync('./data.txt', 'utf8');
+    let number = parseInt(data);
+    number++;
+    fs.writeFile('./data.txt', number, function() {
+      io.emit('updateCounter', { data: number });
+      res.redirect('/');
+    });
+  }
 }
 
 exports.readFile = (req, res) => {
