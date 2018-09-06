6972047ee7f3f51fa8ac07c942f6ebdbdc4a4d52
AntonEmery <antonemery@gmail.com>
2018-09-06 10:48:33 -0700

    fixes

--- controllers/countController.js
+++ controllers/countController.js
@@ -6,7 +6,7 @@ exports.increment = (req, res) => {
   // if the number input contains a number
   if(req.body.custom_number) {
     // write that number to the text file
-    fs.writeFile(__dirname + '/../public/data.txt', req.body.custom_number, function() {
+    fs.writeFile(__dirname + '/../../data.txt', req.body.custom_number, function() {
       // push that number to the view page via socket.io
       io.emit('updateCounter', { data: req.body.custom_number });
       // reload the home page
@@ -16,13 +16,13 @@ exports.increment = (req, res) => {
     // if the input does not contain a number then they must have wanted to increment it.
   } else {
     // read current number from the text file
-    let data = fs.readFileSync(__dirname + '/../public/data.txt', 'utf8');
+    let data = fs.readFileSync(__dirname + '/../../data.txt', 'utf8');
     // turn it into a number
     let number = parseInt(data);
     // increase it by one
     number++;
     // write the incremented number back to the text file, push it out to the view page, reload the home page
-    fs.writeFile(__dirname + '/../public/data.txt', number, function() {
+    fs.writeFile(__dirname + '/../../data.txt', number, function() {
       io.emit('updateCounter', { data: number });
       res.redirect('/');
     });
@@ -30,6 +30,6 @@ exports.increment = (req, res) => {
 }
 
 exports.viewPage = (req, res) => {
-  let number = fs.readFileSync(__dirname + '/../public/data.txt', 'utf8');
+  let number = fs.readFileSync(__dirname + '/../../data.txt', 'utf8');
   res.render('view', { number })
 }
