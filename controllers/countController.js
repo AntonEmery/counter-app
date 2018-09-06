78b8d7adaf63f9374ed31ec30ce5bec4ab328be4
AntonEmery <antonemery@gmail.com>
2018-09-06 16:07:22 -0700

    successfully writing to text file from exe

--- controllers/countController.js
+++ controllers/countController.js
@@ -1,4 +1,9 @@
-const fs = require('fs');
+// const fs = require('fs');
+
+const r = function() { return require(arguments[0][0]) };
+const pathFor = (...args) => path.join(...args)
+const [os, fs, path] = [r`os`, r`fs`, r`path`];
+const home = os.homedir();
 
 exports.increment = (req, res) => {
   // allows use of socket.io in routes
@@ -6,7 +11,7 @@ exports.increment = (req, res) => {
   // if the number input contains a number
   if(req.body.custom_number) {
     // write that number to the text file
-    fs.writeFile(__dirname + '/../../data.txt', req.body.custom_number, function() {
+    fs.writeFile(pathFor(home + '/data.txt'), req.body.custom_number, function() {
       // push that number to the view page via socket.io
       io.emit('updateCounter', { data: req.body.custom_number });
       // reload the home page
@@ -16,13 +21,13 @@ exports.increment = (req, res) => {
     // if the input does not contain a number then they must have wanted to increment it.
   } else {
     // read current number from the text file
-    let data = fs.readFileSync(__dirname + '/../../data.txt', 'utf8');
+    let data = fs.readFileSync(pathFor(home + '/data.txt'), 'utf8');
     // turn it into a number
     let number = parseInt(data);
     // increase it by one
     number++;
     // write the incremented number back to the text file, push it out to the view page, reload the home page
-    fs.writeFile(__dirname + '/../../data.txt', number, function() {
+    fs.writeFile(pathFor(home + '/data.txt'), number, function() {
       io.emit('updateCounter', { data: number });
       res.redirect('/');
     });
@@ -30,6 +35,6 @@ exports.increment = (req, res) => {
 }
 
 exports.viewPage = (req, res) => {
-  let number = fs.readFileSync(__dirname + '/../../data.txt', 'utf8');
+  let number = fs.readFileSync(pathFor(home + '/data.txt'), 'utf8');
   res.render('view', { number })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const countController = require('../controllers/countController');
-const fs = require('fs');
+// const fs = require('fs');
+
+const r = function() { return require(arguments[0][0]) };
+const pathFor = (...args) => path.join(...args)
+const [os, fs, path] = [r`os`, r`fs`, r`path`];
+const home = os.homedir();
 
 // load the home page
 router.get('/', function(req, res) {
-  let data = fs.readFileSync(__dirname + '/../../data.txt', 'utf8');
+  let data = fs.readFileSync(pathFor(home + '/data.txt'), 'utf8');
   let number = parseInt(data);
   res.render('index', { number });
 })
