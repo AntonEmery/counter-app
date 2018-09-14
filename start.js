32647af9e391deb178f8a4d18b5c0c58e4f2376f
AntonEmery <antonemery@gmail.com>
2018-09-14 08:20:50 -0700

    displaying ipadress on app start

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const ip = require('ip')
 
 app.set('port', process.env.port || 7777);
 const server = app.listen(7777, ip.address(), () => {
-  console.log(`App running on port ${server.address().port}`);
+  console.log(`App running at ${ip.address()}:7777 and ${ip.address()}:7777/view` );
 })
 
 const io = require('socket.io')(server);
